5fffe380869118c2f48f4123e6fdc128bdb5e067
DiegoLibonati <diego.libonati1998@gmail.com>
2024-06-21 23:40:15 -0300

    antialiased added to font style

--- filemanager-app/src/app/layout.tsx
+++ filemanager-app/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`flex relative flex-col lg:flex-row h-full min-h-screen w-full ${inter.className}`}
+        className={`flex relative flex-col lg:flex-row h-full min-h-screen w-full ${inter.className} antialiased`}
       >
         <ProviderRedux>{children}</ProviderRedux>
       </body>
