cd8d6e70f7e8b745f4eae06fdffc3d49fb093f41
DiegoLibonati <diego.libonati1998@gmail.com>
2025-01-19 10:40:21 -0300

    feat: better css config and tests config

---
+++ filemanager-app/src/app/lib/configCss.ts
@@ -0,0 +1,10 @@
+export const rootCss = {
+  colors: {
+    primary: "#8357fe",
+    secondary: "#fc8efe",
+    black: "#000000",
+    white: "#FFFFFF",
+    lightGray: "#777",
+    bodyBackground: "#fbfbfb",
+  },
+};
--- filemanager-app/tailwind.config.ts
+++ filemanager-app/tailwind.config.ts
@@ -12,6 +12,8 @@ const config: Config = {
       colors: {
         primary: "#8357fe",
         secondary: "#fc8efe",
+        black: "#000000",
+        white: "#FFFFFF",
         lightGray: "#777",
         bodyBackground: "#fbfbfb",
       },
